5223d648a3228d4c6ac8ec0ad6fc7170f5aeac4b
juanf <yioyiomain@gmail.com>
2024-05-21 16:25:08 -0500

    Guardando homework en archivo course

--- src/models/Course.py
+++ src/models/Course.py
@@ -15,3 +15,32 @@ class Course(Base):
         "Student", secondary="student_course", back_populates="courses"
     )
     homeworks = relationship("Homework", back_populates="course")
+
+class Homework(Base):
+    __tablename__ = "homeworks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String(500))
+    due_date = Column(DateTime, nullable=False)
+    is_active = Column(Boolean, default=True)
+    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+    course = relationship("Course", back_populates="homeworks")
+    type = Column(String(50))
+
+    __mapper_args__ = {
+        "polymorphic_identity": "assignment",
+        "polymorphic_on": type,
+    }
+
+
+class Exam(Homework):
+    __tablename__ = "exams"
+
+    id = Column(Integer, ForeignKey("assignments.id"), primary_key=True)
+    course = relationship("Course", back_populates="exams")
+    duration = Column(Integer)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "exam",
+    }
